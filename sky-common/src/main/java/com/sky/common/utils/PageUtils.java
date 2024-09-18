0d675a31b00d59bf8b7379b8700242229a684e35
xianzicai <xianzhiyong763@gmail.com>
2024-09-18 11:26:54 +0800

    二次开发-项目名修改

---
+++ sky-common/src/main/java/com/sky/common/utils/PageUtils.java
@@ -0,0 +1,39 @@
+package com.sky.common.utils;
+
+import com.github.pagehelper.PageHelper;
+import com.sky.common.core.page.PageDomain;
+import com.sky.common.core.page.TableSupport;
+import com.sky.common.utils.sql.SqlUtil;
+
+/**
+ * 分页工具类
+ * 
+ * @author ruoyi
+ */
+public class PageUtils extends PageHelper
+{
+    /**
+     * 设置请求分页数据
+     */
+    public static void startPage()
+    {
+        // 创建一个 PageDomain 对象，用于接收前端分页的参数
+        PageDomain pageDomain = TableSupport.buildPageRequest();
+        // 从 PageDomain 对象中提取出当前页码、每页大小、排序条件
+        Integer pageNum = pageDomain.getPageNum();
+        Integer pageSize = pageDomain.getPageSize();
+        // 对排序条件进行转义处理，防止 SQL 侏儒
+        String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
+        Boolean reasonable = pageDomain.getReasonable();
+        // 使用 PageHelper 开启分页，设置页码、每页大小、排序条件、是否启用合理化分页
+        PageHelper.startPage(pageNum, pageSize, orderBy).setReasonable(reasonable);
+    }
+
+    /**
+     * 清理分页的线程变量
+     */
+    public static void clearPage()
+    {
+        PageHelper.clearPage();
+    }
+}
